bb7aa52f20ca3f70fed96b4e91e4ac73311aeda9
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-23 15:39:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,2 @@
-class Dog
-  def initialize(name, breed = "Mutt")
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-end
 
-fido = Dog.new("Fido", "Pug")
 
